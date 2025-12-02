a802c08dd2f5feac54b1c75ecf5656e45f77504b
al45773-oss <al45773@un.edu.mx>
2025-12-02 10:11:36 -0600

    java

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 let esMasculino = true;
- 
+
+const card = documentquerySelector (".perfil-card");
+const nombre = document.getElementById ("nombre");
+const ocupacion = document.getElementById ("ocupacion")
+
+card.addEventListener ("click",()=> 
+if (esMasculino)
+
+{
+    nombre.textContent = "Andrea Pierinaa Gignac";
+    ocupacion.textContent = "Futbolista Delantera (version femenina)";
+    
+}
